09e436217aba901d347ffdd3c1d0f07c379d3606
Mowlid Sead <126351251+MowlidSead0@users.noreply.github.com>
2023-07-26 15:36:07 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:
+      {
+        'eee-blue':'#1E90FF',
+        'publue': '#5E76FF',
+        'degi-100': '#39CA5D',
+        'puby': '#0271DD',
+        'tgu': '#FD1786',
+        'yellow': '#FDE508',
+        'punk':'rgb(246, 153, 150)',
+      },
+    },
+  },
+  plugins: [],
+}
